a301e4872d63106156dd2aba7025a49c6a4914c0
Misko Lee <misko.lee@bindo.com>
2020-10-27 22:47:48 +0800

    1. impl http client example

--- 003_add_http_client_to_javascript/http_client.go
+++ 003_add_http_client_to_javascript/http_client.go
@@ -1,9 +1,27 @@
 package main
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
 type HttpClient struct {
 
 }
 
-func (h *HttpClient) Get(url string) (string,error) {
-	return "todo",nil
+func (h *HttpClient) Get(url string) string {
+	resp,err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body,err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
+}
+
+func NewHttpClient() *HttpClient {
+	return &HttpClient{}
 }
--- 003_add_http_client_to_javascript/main.go
+++ 003_add_http_client_to_javascript/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "github.com/robertkrimen/otto"
 
+func NewVM() *otto.Otto {
+	vm :=  otto.New()
+	vm.Set("NewHttpClient",NewHttpClient)
+	return vm
+}
+
+func main() {
+	vm := NewVM()
+	script := `
+	var httpClient = NewHttpClient();
+	var respBody = httpClient.Get("https://www.baidu.com");
+	console.log(respBody);
+`
+	if _,err := vm.Run(script); err != nil {
+		panic("Can not run script")
+	}
 }
